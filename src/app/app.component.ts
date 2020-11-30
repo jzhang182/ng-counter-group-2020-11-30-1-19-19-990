f4edf2c2251c3d8c86505bbdd8fd44886451d2a3
Ge Luxue <lge5@slb.com>
2020-11-30 14:29:42 +0800

    feat: pass counter from appcomponent to countercomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Counter} from './models/counter';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-counter-group';
+  public counter = new Counter();
 }
