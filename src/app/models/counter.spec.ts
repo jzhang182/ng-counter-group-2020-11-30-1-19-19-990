c30e369c9f912fdfa373c2942db5f1caf7161150
Ge Luxue <lge5@slb.com>
2020-11-30 13:38:04 +0800

    feat: implement increase/decrease of model/counter

--- src/app/models/counter.spec.ts
+++ src/app/models/counter.spec.ts
@@ -1,7 +1,38 @@
 import { Counter } from './counter';
 
 describe('Counter', () => {
-  it('should create an instance', () => {
-    expect(new Counter()).toBeTruthy();
+  it('should set account when create an instance', () => {
+    // given
+    const account = 10;
+
+    // when
+    const counter = new Counter(account);
+
+    // then
+    expect(counter.account).toBe(10);
+  });
+
+  it('should increase account by 1 when call increase', () => {
+    // given
+    const account = 10;
+
+    // when
+    const counter = new Counter(account);
+    counter.increase();
+
+    // then
+    expect(counter.account).toBe(account + 1);
+  });
+
+  it('should decrease account by 1 when call decrease', () => {
+    // given
+    const account = 10;
+
+    // when
+    const counter = new Counter(account);
+    counter.decrease();
+
+    // then
+    expect(counter.account).toBe(account - 1);
   });
 });
--- src/app/models/counter.ts
+++ src/app/models/counter.ts
@@ -1,2 +1,14 @@
 export class Counter {
+  public account: number;
+  constructor(account: number = 0){
+    this.account = account;
+  }
+
+  public increase(): void{
+    this.account ++;
+  }
+
+  public decrease(): void{
+    this.account --;
+  }
 }
